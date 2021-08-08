ee410a9f342be8cfe093368ff9d00d6f07ed2666
Ikemoto-Yu <cosper79@gmail.com>
2021-08-08 11:13:20 +0900

    ユーザー登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         with_options presence: true do
+           validates :nickname
+           validates :email
+           validates :password
+           validates :password_confirmation
+             with_options  format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+             validates :first_name
+             validates :last_name
+             end
+         end
 end
