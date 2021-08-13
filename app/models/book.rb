d2dff709e2515ac1dcb627e23d0f3306ff155e46
Ikemoto-Yu <cosper79@gmail.com>
2021-08-13 21:08:42 +0900

    コメント投稿機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many :comments , dependent: :destroy
   with_options presence: true do
     validates :info
     validates :name
