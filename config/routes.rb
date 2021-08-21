8106cd77082c555fb4173b6e368e1b8bd9bebf9c
Ikemoto-Yu <cosper79@gmail.com>
2021-08-21 13:09:06 +0900

    snsログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: 'books#index'
   resources :books  do
     resources :orders , only: [:index, :create]
